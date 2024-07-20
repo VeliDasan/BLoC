9375821ed8cb1e2099a920514c2089dbe5fb51b2
Veli DAŞAN <veli6343@hotmail.com>
2024-07-20 19:04:59 +0300

    gauge

--- lib/src/pages/vehicleDetail.dart
+++ lib/src/pages/vehicleDetail.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:kdgaugeview/kdgaugeview.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 import '../blocs/detailBLoc/detail_bloc.dart';
 import '../blocs/detailBLoc/detail_event.dart';
 import '../blocs/detailBLoc/detail_state.dart';
@@ -12,7 +14,7 @@ import '../elements/pageLoading.dart';
 class VehicleDetailScreen extends StatefulWidget {
   final int deviceId;
 
-  const VehicleDetailScreen({required this.deviceId});
+  const VehicleDetailScreen({super.key, required this.deviceId});
 
   @override
   State<VehicleDetailScreen> createState() => _VehicleDetailScreenState();
@@ -43,7 +45,7 @@ class _VehicleDetailScreenState extends State<VehicleDetailScreen> {
         providers: [
           BlocProvider(
             create: (context) =>
-            DetailBloc()..add(GetVehicleDetail(deviceId: widget.deviceId)),
+                DetailBloc()..add(GetVehicleDetail(deviceId: widget.deviceId)),
           ),
           BlocProvider(
             create: (context) => MapBloc(),
@@ -55,69 +57,176 @@ class _VehicleDetailScreenState extends State<VehicleDetailScreen> {
             if (state is DetailLoadingState) {
               return pageLoading();
             } else if (state is DetailSuccessState) {
-              return Padding(
+              return SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
                 padding: const EdgeInsets.all(16.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Device ID: ${state.vehicleDetailData.deviceId}'),
-                    Text('Fuel Tank Level: ${state.vehicleDetailData.fuelTankLevel}%'),
-                    Text('Longitude: ${state.vehicleDetailData.longitude}'),
-                    Text('Latitude: ${state.vehicleDetailData.latitude}'),
-                    Text('KM: ${state.vehicleDetailData.km}'),
-                    Text('Speed: ${state.vehicleDetailData.speed} km/h'),
-                    BlocBuilder<MapBloc, MapState>(
-                      builder: (context, mapState) {
-                        if (mapState is MapVisibleState) {
-                          return Container(
-                            height: 300,
-                            width: double.infinity,
-                            child: Padding(
-                              padding: const EdgeInsets.all(15.0),
-                              child: GoogleMap(
-                                onMapCreated: _onMapCreated,
-                                initialCameraPosition: CameraPosition(
-                                  target: LatLng(
-                                    state.vehicleDetailData.latitude,
-                                    state.vehicleDetailData.longitude,
-                                  ),
-                                  zoom: 14,
-                                ),
-                                markers: {
-                                  Marker(
-                                    markerId:const MarkerId('vehicle_location'),
-                                    position: LatLng(
-                                      state.vehicleDetailData.latitude,
-                                      state.vehicleDetailData.longitude,
-                                    ),
-                                    infoWindow:const InfoWindow(title: 'Vehicle Location'),
-                                  ),
-                                },
-                              ),
+                  children: <Widget>[
+                    Card(
+                      elevation: 4,
+                      margin: const EdgeInsets.symmetric(vertical: 10),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                const Icon(Icons.directions_car),
+                                const SizedBox(width: 8),
+                                Text(
+                                    'Device ID: ${state.vehicleDetailData.deviceId}'),
+                              ],
                             ),
-                          );
-                        }
-                        return Container();
-                      },
+                            Row(
+                              children: [
+                                const Icon(Icons.local_gas_station),
+                                const SizedBox(width: 8),
+                                Text(
+                                    'Fuel Tank Level: ${state.vehicleDetailData.fuelTankLevel}%'),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Icon(Icons.location_on),
+                                const SizedBox(width: 8),
+                                Text(
+                                    'Longitude: ${state.vehicleDetailData.longitude}'),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Icon(Icons.location_on),
+                                const SizedBox(width: 8),
+                                Text(
+                                    'Latitude: ${state.vehicleDetailData.latitude}'),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Icon(Icons.speed),
+                                const SizedBox(width: 8),
+                                Text('KM: ${state.vehicleDetailData.km}'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                    locationButton(
-                      onPressed: () {
-                        context.read<MapBloc>().add(ToggleMapVisibility(
-                          latitude: state.vehicleDetailData.latitude,
-                          longitude: state.vehicleDetailData.longitude,
-                        ));
-                        _moveToVehicleLocation(
-                          state.vehicleDetailData.latitude,
-                          state.vehicleDetailData.longitude,
-                        );
-                      },
-                      title: "Konuma Git",
+                    Card(
+                      elevation: 4,
+                      margin: const EdgeInsets.symmetric(vertical: 10),
+                      child: Column(
+                        children: [
+                          BlocBuilder<MapBloc, MapState>(
+                            builder: (context, mapState) {
+                              if (mapState is MapVisibleState) {
+                                return SizedBox(
+                                  height: 300,
+                                  width: double.infinity,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(15.0),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: GoogleMap(
+                                        onMapCreated: _onMapCreated,
+                                        initialCameraPosition: CameraPosition(
+                                          target: LatLng(
+                                            state.vehicleDetailData.latitude,
+                                            state.vehicleDetailData.longitude,
+                                          ),
+                                          zoom: 14,
+                                        ),
+                                        markers: {
+                                          Marker(
+                                            markerId: const MarkerId(
+                                                'vehicle_location'),
+                                            position: LatLng(
+                                              state.vehicleDetailData.latitude,
+                                              state.vehicleDetailData.longitude,
+                                            ),
+                                            infoWindow: const InfoWindow(
+                                                title: 'Vehicle Location'),
+                                          ),
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              }
+                              return Container();
+                            },
+                          ),
+                          locationButton(
+                            onPressed: () {
+                              context.read<MapBloc>().add(ToggleMapVisibility(
+                                    latitude: state.vehicleDetailData.latitude,
+                                    longitude:
+                                        state.vehicleDetailData.longitude,
+                                  ));
+                              _moveToVehicleLocation(
+                                state.vehicleDetailData.latitude,
+                                state.vehicleDetailData.longitude,
+                              );
+                            },
+                            title: "Konuma Git",
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: SizedBox(
+                        width: 400,
+                        height: 400,
+                        child: KdGaugeView(
+                          minSpeed: 0,
+                          maxSpeed: 200,
+                          speed: state.vehicleDetailData.speed,
+                          animate: true,
+                          duration: const Duration(seconds: 1),
+                          alertSpeedArray: const [40, 80, 90],
+                          alertColorArray: const [
+                            Colors.orange,
+                            Colors.indigo,
+                            Colors.red
+                          ],
+                        ),
+                      ),
+                    ),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.local_gas_station, color: Colors.red),
+                        Icon(Icons.local_gas_station, color: Colors.green),
+                      ],
+                    ),
+                    SfLinearGauge(
+                      ranges: const [
+                        LinearGaugeRange(
+                          startValue: 0,
+                          endValue: 100,
+                          color: Colors.grey,
+                        ),
+                      ],
+                      markerPointers: [
+                        LinearShapePointer(
+                          value: state.vehicleDetailData.fuelTankLevel,
+                        ),
+                      ],
+                      barPointers: [
+                        LinearBarPointer(
+                          value: state.vehicleDetailData.fuelTankLevel,
+                          color: Colors.blue,
+                        ),
+                      ],
                     ),
                   ],
                 ),
               );
             } else if (state is DetailErrorState) {
-              return const Center(child: Text('Detay Yüklenirken Hata Oluştu'));
+              return const Center(child: Text('Error'));
             }
             return Container();
           },
