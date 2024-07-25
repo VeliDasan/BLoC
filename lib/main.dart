a485f4735284410458f79a3b9a952d1f8f07a33f
Veli DAŞAN <veli6343@hotmail.com>
2024-07-25 09:57:11 +0300

    LoginFirebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:bloc_yapisi/src/blocs/weatherBLoC/weather_bloc.dart';
 import 'package:bloc_yapisi/src/pages/login.dart';
-import 'package:bloc_yapisi/src/repositories/weather_repository.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'src/pages/list.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
